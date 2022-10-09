b8fcaf428780112f41ef743d22e82e696964c66b
Ashish Sharda <ashishjsharda@gmail.com>
2022-10-09 05:52:50 -0500

    Add Exerice File

---
+++ src/components/Exercise1-03.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Exercise 1-03</h1>
+    <p>Click the button to change the text.</p>
+    <button @click="changeText">Change Text</button>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Exercise1-03"
+}
+</script>
+
+<style scoped>
+
+</style>
